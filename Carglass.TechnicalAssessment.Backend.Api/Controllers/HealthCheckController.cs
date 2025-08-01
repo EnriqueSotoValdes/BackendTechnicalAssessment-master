921074aa8825d7a65b05538edd4c6a4734977c55
enrique soto valdes <u45335324@gmail.com>
2025-08-01 19:53:33 +0200

    Crear controlador Health Check

---
+++ Carglass.TechnicalAssessment.Backend.Api/Controllers/HealthCheckController.cs
@@ -0,0 +1,15 @@
+﻿using Carglass.TechnicalAssessment.Backend.BL;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Carglass.TechnicalAssessment.Backend.Api.Controllers;
+
+[ApiController]
+[Route("healthCHeck")]
+public class HealthCheckController : ControllerBase
+{
+    [HttpGet]
+    public IActionResult KeepAlive()
+    {
+        return Ok();
+    }
+}
